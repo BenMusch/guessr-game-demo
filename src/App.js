5087a8de756465a3f612cef233b343a1caa335bc
Ben Muschol <benjamin.muschol@airtable.com>
2023-08-07 23:17:14 -0400

    Install mapbox, render basic map
    Adds necessary dependencies to get mapbox running, but doesn't have any real interactivity

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import "./App.css";
 
+import { Map } from "react-map-gl";
+
+const initialViewState = {
+  zoom: 10,
+  latitude: 41.8781,
+  longitude: -87.6298,
+};
+
+function GameplayMap() {
+  return (
+    <Map
+      style={{ width: 600, height: 400 }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      initialViewState={initialViewState}
+      maxZoom={12}
+      minZoom={8.5}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
-      <h1>We have a blank canvas!</h1>
+      <GameplayMap />
     </div>
   );
 }
